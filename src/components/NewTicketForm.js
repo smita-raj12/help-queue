923afc6cb1e84b457d51ac4f121998a2085e5a44
smita <smita.raj12@gmail.com>
2022-01-04 17:12:01 -0800

    application for tickets

---
+++ src/components/NewTicketForm.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { v4 } from 'uuid'; 
+
+function NewTicketForm(){
+    function handleNewTicketFormSubmission(event) {
+        event.preventDefault();
+        console.log(event.target.names.value);
+        console.log(event.target.location.value);
+        console.log(event.target.issue.value);
+      }
+    return (
+        <React.Fragment>
+          <form onSubmit={handleNewTicketFormSubmission}>
+            <input
+              type='text'
+              name='names'
+              placeholder='Pair Names' />
+            <input
+              type='text'
+              name='location'
+              placeholder='Location' />
+            <textarea
+              name='issue'
+              placeholder='Describe your issue.' />
+            <button type='submit'>Help!</button>
+          </form>
+        </React.Fragment>
+      );
+}
+
+export default NewTicketForm;
