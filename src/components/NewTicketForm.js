c54e2a458dbceab6b4f467773cfc711dedcce61d
smita <smita.raj12@gmail.com>
2022-01-05 20:51:29 -0800

    update the help queue

--- src/components/NewTicketForm.js
+++ src/components/NewTicketForm.js
@@ -1,13 +1,20 @@
 import React from "react";
 import { v4 } from 'uuid'; 
+import PropTypes from "prop-types";
 
-function NewTicketForm(){
+function NewTicketForm(props){
     function handleNewTicketFormSubmission(event) {
         event.preventDefault();
         console.log(event.target.names.value);
         console.log(event.target.location.value);
         console.log(event.target.issue.value);
       }
+
+      function handleNewTicketFormSubmission(event) {
+        event.preventDefault();
+        props.onNewTicketCreation({names: event.target.names.value, location: event.target.location.value, issue: event.target.issue.value, id: v4()});
+      }
+      
     return (
         <React.Fragment>
           <form onSubmit={handleNewTicketFormSubmission}>
@@ -27,5 +34,9 @@ function NewTicketForm(){
         </React.Fragment>
       );
 }
+
+NewTicketForm.propTypes = {
+  onNewTicketCreation: PropTypes.func
+};
 
 export default NewTicketForm;
--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -7,24 +7,31 @@ class TicketControl extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            formVisibleOnPage: false
+            formVisibleOnPage: false,
+            mainTicketList: []
         };
         this.handleClick = this.handleClick.bind(this);
     }
+
     handleClick  () {
         this.setState(prevState => ({
             formVisibleOnPage: !prevState.formVisibleOnPage
         }));
     }
 
+    handleAddingNewTicketToList = (newTicket) => {
+      const newMainTicketList = this.state.mainTicketList.concat(newTicket);
+      this.setState({mainTicketList: newMainTicketList,
+                    formVisibleOnPage: false });
+    }
     render(){
         let currentlyVisibleState = null;
         let buttonText = null; // new code
         if (this.state.formVisibleOnPage) {
-          currentlyVisibleState = <NewTicketForm />;
+          currentlyVisibleState = <NewTicketForm onNewTicketCreation={this.handleAddingNewTicketToList} />;
           buttonText = "Return to Ticket List"; // new code
         } else {
-          currentlyVisibleState = <TicketList />;
+          currentlyVisibleState = <TicketList ticketList={this.state.mainTicketList} />;
           buttonText = "Add Ticket"; // new code
         }
         return (
