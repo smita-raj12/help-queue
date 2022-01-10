7f2fb04cc3b40d24d10b7a796ed1f4e7ce599cc8
smita <smita.raj12@gmail.com>
2022-01-09 16:54:53 -0800

    add full curd functionality

--- src/components/NewTicketForm.js
+++ src/components/NewTicketForm.js
@@ -1,36 +1,19 @@
 import React from "react";
 import { v4 } from 'uuid'; 
 import PropTypes from "prop-types";
+import ReusableForm from "./ReusableForm";
 
 function NewTicketForm(props){
-    function handleNewTicketFormSubmission(event) {
-        event.preventDefault();
-        console.log(event.target.names.value);
-        console.log(event.target.location.value);
-        console.log(event.target.issue.value);
-      }
-
       function handleNewTicketFormSubmission(event) {
         event.preventDefault();
         props.onNewTicketCreation({names: event.target.names.value, location: event.target.location.value, issue: event.target.issue.value, id: v4()});
       }
       
-    return (
+      return (
         <React.Fragment>
-          <form onSubmit={handleNewTicketFormSubmission}>
-            <input
-              type='text'
-              name='names'
-              placeholder='Pair Names' />
-            <input
-              type='text'
-              name='location'
-              placeholder='Location' />
-            <textarea
-              name='issue'
-              placeholder='Describe your issue.' />
-            <button type='submit'>Help!</button>
-          </form>
+          <ReusableForm 
+            formSubmissionHandler={handleNewTicketFormSubmission}
+            buttonText="Help!" />
         </React.Fragment>
       );
 }
